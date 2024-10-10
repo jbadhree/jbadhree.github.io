8fff17d66de3ff6bb79bd500f0c5c41d1923b40c
Badhree Babu <jbadhree@gmail.com>
2024-10-10 10:01:22 -0400

    adding * to ignore patterns

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "badhreebabu.com",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private_*", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
