1b2e49fd601c4725d7547905367f4f16329e8a38
Badhree Babu <jbadhree@gmail.com>
2024-10-10 10:43:08 -0400

    Trying comments from rick

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,22 +5,7 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  footer: Component.Footer(),
-  afterBody: [
-    Component.Comments({
-      provider: 'giscus',
-      options: {
-        // from data-repo
-        repo: 'jbadhree/jbadhree.github.io',
-        // from data-repo-id
-        repoId: 'MDEwOlJlcG9zaXRvcnkyMjgyMzkyNTc=',
-        // from data-category
-        category: 'Announcements',
-        // from data-category-id
-        categoryId: 'DIC_kwDODZqnmc4CjJWt',
-      }
-    } as any), 
-  ],
+  footer: Component.Comments(),
 }
 
 // components for pages that display a single page (e.g. a single note)
--- quartz/components/Comments.tsx
+++ quartz/components/Comments.tsx
@@ -1,8 +1,13 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import style from "./styles/comments.scss"
 import script from "./scripts/giscus.inline"
-import { Options } from "reading-time"
 
+// Define the Options just to satisfy Quartz
+type Options = {
+    property1: string;
+    property2: string;
+  }
+  
 export default ((opts?: Options) => {
   const Comments: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
     return (
